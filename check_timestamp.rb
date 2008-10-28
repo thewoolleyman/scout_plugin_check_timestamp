ce6228f63ef33bb976422a59dcd65f827927abc0
Chad Woolley <thewoolleyman@gmail.com>
2008-10-28 14:31:39 -0700

    fix report

--- check_timestamp.rb
+++ check_timestamp.rb
@@ -5,9 +5,9 @@ class CheckTimestamp < Scout::Plugin
       path = option(:path)
       timestamp = File.new(path).mtime
       current_time = Time.now
-      difference = (current_time - timestamp) / 60
-      output = "Path: #{path}, Threshold: #{threshold}, Timestamp: #{timestamp}, Current Time: #{current_time}, Difference: #{difference}"
-      report(:output => output)
+      difference = ((current_time - timestamp) / 60).round
+      output = "Path: #{path}, Threshold: #{threshold} minutes, Path Timestamp: #{timestamp}, Current Time: #{current_time}, Difference: #{difference}"
+      report(:difference => difference, :threshold => threshold)
       if difference > threshold
         alert(:subject => "File #{path} was older than #{threshold} minutes", :body => output)
       end
